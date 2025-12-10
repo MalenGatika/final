1ad7b00699db374cb04235677922f58e3fa081c3
MalenGatika <malengatika@gmail.com>
2025-12-09 21:37:51 -0300

    limpiando

--- michael/js/michael.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("navToggle");
-  const menu = document.getElementById("navMenu");
-
-  btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-  });
-});
